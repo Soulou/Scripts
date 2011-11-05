57eacd3b984165d09f6e5c9b14dc413cb721ab01
Unbekandt Léo <leo@soulou.fr>
2011-11-05 14:49:18 +0100

    Add a script able to manage power states of your AMD graphic card if you're using the radeon driver

---
+++ gui_amdgpu_powerstate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+suer=""
+
+if [ $UID -ne 0 ] ; then
+	if [ "$TERM" = "dumb" ] ; then
+		if [ -x "/usr/bin/gksudo" ] ; then
+			suer="/usr/bin/gksudo"
+		elif [ -x "/usr/bin/gksu" ] ; then
+			suer="/usr/bin/gksu"
+		fi
+	elif [ -x "/usr/bin/sudo" ] ; then
+		suer="/usr/bin/sudo"
+	else
+		echo "You have to be root !"
+	fi
+	$suer "$0"
+	exit 0
+fi
+
+powerlevel=`cat /sys/class/drm/card0/device/power_profile`
+
+for i in `seq 0 1 4` ; do actives[$i]=FALSE ; done
+
+case "$powerlevel" in
+"default")
+	actives[0]="TRUE";;
+"auto")
+	actives[1]="TRUE";;
+"high")
+	actives[2]="TRUE";;
+"mid")
+	actives[3]="TRUE";;
+"low")
+	actives[4]="TRUE";;
+*)
+	;;
+esac
+
+ret=`zenity --list --radiolist --column "Choice" --column "Power Level" --width 100 --height 300 ${actives[0]} "default" ${actives[1]} "auto" ${actives[2]} "high" ${actives[3]} "med" ${actives[4]} "low"`
+
+if [ -n "$ret" ] ; then
+	echo $ret > /sys/class/drm/card0/device/power_profile
+fi
